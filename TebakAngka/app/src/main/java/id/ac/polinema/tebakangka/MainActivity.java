3cf19f01d03f15933d45e470e4fa9192a9755185
Dian Hanifudin Subhi <dhanifudin@gmail.com>
2019-09-01 17:32:15 +0700

    add tebak angka starter

---
+++ TebakAngka/app/src/main/java/id/ac/polinema/tebakangka/MainActivity.java
@@ -0,0 +1,36 @@
+package id.ac.polinema.tebakangka;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+	//  TODO: deklarasikan variabel di sini
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		// TODO: bind layout di sini
+	}
+
+	// TODO: generate angka random di sini
+	private void initRandomNumber() {
+
+	}
+
+	public void handleGuess(View view) {
+		// TODO: Tambahkan logika untuk melakukan pengecekan angka
+	}
+
+	public void handleReset(View view) {
+		// TODO: Reset tampilan
+	}
+}
